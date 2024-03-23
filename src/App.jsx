841a9cc906dc394ab3723b8e0bc26d71d603e729
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-23 20:21:11 -0300

    se protegieron las rutas de usuarios y recetas del administrador

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/pages/Inicio";
-import Administrador from "./components/pages/Administrador";
-import FormularioReceta from "./components/pages/receta/FormularioReceta";
 import Error404 from "./components/pages/Error404";
 import DetalleReceta from "./components/pages/DetalleReceta";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -30,21 +28,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Inicio></Inicio>}></Route>
           <Route
-  exact
-  path="/administrador/usuarios"
-  element={<Administrador tipo="usuarios" />}
-/>
-<Route
-  exact
-  path="/administrador/recetas"
-  element={<Administrador tipo="recetas" />}
-/>
-          <Route
             exact
             path="/administrador/*"
             element={
               <RutasProtegidas>
-                <RutasAdmin></RutasAdmin>
+                <RutasAdmin usuarioLogueado={usuarioLogueado}></RutasAdmin>
               </RutasProtegidas>
             }
           ></Route>
@@ -53,15 +41,15 @@ function App() {
             path="/detalle/:id"
             element={<DetalleReceta></DetalleReceta>}
           ></Route>
-           <Route
+          <Route
             exact
-            path="/administrador/usuarios/crear"
-            element={<FormularioRegistro editar={false} titulo="Registro" rol='hidden' usuarioLogueado={usuarioLogueado}></FormularioRegistro>}
-          ></Route>
-           <Route
-            exact
-            path="/administrador/usuarios/editar/:id"
-            element={<FormularioRegistro editar={true} titulo="Editar Usuario" rol=''></FormularioRegistro>}
+            path="/registro"
+            element={
+              <FormularioRegistro
+                editar={false}
+                titulo="Registro"
+              ></FormularioRegistro>
+            }
           ></Route>
           <Route
             exact
